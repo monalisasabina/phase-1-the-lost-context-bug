2a1f356d31bfc969b9c7880a6be28d0be68d71c0
Monalisa Onyango <+>
2024-07-12 19:49:57 +0300

    bug code

--- index.js
+++ index.js
@@ -10,3 +10,85 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+
+// BUGGY CODE
+// .................................................................
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
+
+// .................................................................
+
+// Debugging: Discovering the Nature of the Lost Context Bug
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   console.log("Debug Before forEach: " + this);
+//   this.signatories.forEach(function (signatory) {
+//     console.log("Debug Inside: " + this);
+//     // const message = `${this.closing[signatory]}, ${signatory}`
+//     // console.log(message)
+//   });
+// };
+
+// printCard.call(messageConfig);
+
+// .......................................................................
+// Solution 1: Use a thisArg to avoid the lost context bug
+
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }, this);
+// };
+
+// printCard.call(messageConfig);
+
+// ........................................................................
+// Solution 2: Use a Closure to Regain Access to the Lost Context
+
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const outerContext = this;
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
+
+// ....................................................................
+// Solution 3: Use an Arrow Function Expression to Create a Function Without Its Own Context
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+    // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+    // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
